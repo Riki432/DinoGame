105dee425c95751f06748d07d7608dc68cd4ccf5
unknown <ashishbawlekar1999@gmail.com>
2019-12-21 14:37:26 +0530

    Added moon,road

--- Road.js
+++ Road.js
@@ -1,5 +1,26 @@
 class Road{
      render(){
-        line(0,height-10,width-10,height-10);
+        fill(0);
+        strokeWeight(10);
+        fill("black");
+        stroke(255,0,0,255);
+        line(0,height-100,width-1,height-100);
+        stroke(255,0,0,0);
+        rect(0,0,width-1,height-100);
+        strokeWeight(0);
+        fill("white");
+        ellipse(width-100, 100, 50,50);
+        
+    
     }
-}
+    DrawStars(i){
+         image(i,0,0,1200,450);
+         
+        
+        }
+    
+
+    }
+
+        
+
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var p;
 var R;
+var starPos = [];
 function setup(){
     createCanvas(1200,550);
-    background(0);
+    background("green");
     p = new Person();
     R= new Road();
 }
+let i;
+
+
+    function preload(){
+        i=loadImage('./images.jpg');
+        
+        
+
+    }
 
 function draw(){
     
     p.render();
     R.render();
+    R.DrawStars(i);
 }
