7a0439ef9de6c4d1769a87c21ba24c26de8f5867
Riki432 <bansoderahul432@gmail.com>
2019-12-21 14:39:24 +0530

    idk

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var p;
 var R;
 var smiley;
+var starPos = [];
 function preload(){
     smiley = loadImage("./smiley.png");
 }
 function setup(){
     createCanvas(1200,550);
+    for(let i = 0; i< 100; i++){
+        starPos.push(
+            createVector(
+                random(0, 1200),
+                random(0, 550)
+            )
+        );
+    }
+    // createCanvas(1200,550);
     background(0);
     p = new Person();
     R = new Road();
@@ -13,10 +23,16 @@ function setup(){
 
 function draw(){
     background(0);
-    
+    //Drawing Stars
+    strokeWeight(4);
+    stroke(255);
+    // for(let star in starPos){
+    //     point(star.x, star.y);
+    // }
+    starPos.forEach(elem => {
+        point(elem.x, elem.y)
+    });
     R.render();
-  
-    
     // p.duck(keyIsDown(DOWN_ARROW));
     // p.jump(keyIsDown(UP_ARROW));
     p.move();
